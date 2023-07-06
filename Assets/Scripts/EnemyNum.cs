cbcf97aeecbb3674b8cc4fd63a3227de5f4b5d7e
HAL <haru.yoshino818@gmail.com>
2023-07-06 15:16:24 +0900

    数値更新のプログラムの準備

---
+++ Assets/Scripts/EnemyNum.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyNum : MonoBehaviour
+{
+
+    public float num;
+
+    Calculation calc;
+
+    void Start()
+    {
+        calc = GameObject.FindWithTag("CalculationManager").GetComponent<Calculation>();
+    }
+
+    void Update()
+    {
+    }
+    void OnDestroy()
+    {
+        calc.receiveNum = num;
+        calc.destroy = true;
+        return;
+    }
+}
